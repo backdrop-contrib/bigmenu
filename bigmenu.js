842e9b36d66e2aad6675545da6793840cdbc4d92
Dan (dman) Morrison <dan@coders.co.nz>
2016-06-17 10:20:42 +1200

    Issue #2748759 : Follow-up additional code cleanups identified by PHP CodeSniffer

--- bigmenu.js
+++ bigmenu.js
@@ -1,22 +1,23 @@
 /**
- * @file add AJAX loading to the manu admin screen
+ * @file Add AJAX loading to the manu admin screen.
  *
  * @author dman dan@coders.co.nz
  */
+
 (function ($) {
 
-Drupal.behaviors.bigmenu = {
-  attach: function(context) {
-    // Add click actions to all the child indicators
-    // The bigmenu-processed class will mark that we don't want to attach this behavior twice.
-    $('form#bigmenu-overview-form .bigmenu-childindictor').not('.bigmenu-processed')
-      .addClass('bigmenu-processed')
-      .click(
+  Drupal.behaviors.bigmenu = {
+    attach: function(context) {
+      // Add click actions to all the child indicators
+      // The bigmenu-processed class will mark that we don't want to attach this behavior twice.
+      $('form#bigmenu-overview-form .bigmenu-childindictor').not('.bigmenu-processed')
+        .addClass('bigmenu-processed')
+        .click(
         function(event){
-          // Don't let the normal href_ click happen
+          // Don't let the normal href_ click happen.
           event.stopPropagation();
 
-          // jquery 1.3+
+          // Jquery 1.3+.
           var parentRow = $(this).parents('tr').get();
 
           // Find the mlid of the cell we are in.
@@ -25,7 +26,7 @@ Drupal.behaviors.bigmenu = {
           // If children have been generated already, just show them again.
           if ($(parentRow).hasClass("bigmenu-generated") && $(parentRow).hasClass("bigmenu-collapsed")) {
             $('.childOf-' + mlid).css('display', '');
-            // Indicate we are expanded now
+            // Indicate we are expanded now.
             $(parentRow)
               .removeClass('bigmenu-collapsed')
               .addClass('bigmenu-expanded');
@@ -34,23 +35,22 @@ Drupal.behaviors.bigmenu = {
             return false;
           }
 
-          // Prevent double-clicks
+          // Prevent double-clicks.
           if ($(parentRow).hasClass("bigmenu-processing")) {
             return;
           }
-          // Set throbber, and indicate we are busy
+          // Set throbber, and indicate we are busy.
           $(parentRow).addClass("bigmenu-processing");
 
-          // We either expand or contract, depending on current status
+          // We either expand or contract, depending on current status.
           if ($(parentRow).hasClass("bigmenu-collapsed")) {
-            // Fetch submenu and expand
-
+            // Fetch submenu and expand.
             // This clicked item has the href call we need to make built in
-            // just add 'js' to the end
+            // just add 'js' to the end.
             var url = $(this).attr('href') + '/js';
 
             // ALSO, to deal with Drupal form API form cache, add the form build ID
-            // so the background process can update the known fields
+            // so the background process can update the known fields.
             var form = $('.menu-mlid', parentRow).attr('form')
             form_id = $('input[name="form_id"]', form).val();
             form_build_id = $('input[name="form_build_id"]', form).val();
@@ -61,33 +61,32 @@ Drupal.behaviors.bigmenu = {
               dataType: 'json',
               url: url,
               success: function(data, textStatus, XMLHttpRequest) {
-                //data = jQuery.parseJson(data);
+                // Data = jQuery.parseJson(data);
                 if (data.status) {
 
                   // Shift the rows into this form.
                   var new_form = $(data.data);
 
                   // Add each tr in the retrieved form  after the parent row
-                  // Tag the added rows so we can find and collapse them later
+                  // Tag the added rows so we can find and collapse them later.
                   var previousRow = parentRow;
                   $('tr', new_form).each(function(index) {
-                      if ($('th', this).length == 0) {
-                        $(this).addClass('childOf-' + mlid)
-                          .css('opacity', 0.2)
-                        //.fadeTo(0, 0.5).css('opacity', 0.1)
-                        $(previousRow).after(this)
+                    if ($('th', this).length == 0) {
+                      $(this).addClass('childOf-' + mlid)
+                        .css('opacity', 0.2)
+                      // .fadeTo(0, 0.5).css('opacity', 0.1)
+                      $(previousRow).after(this)
 
-                        // TODO - an animation of some sort - tr,td cannot set height however
-                        previousRow = this
-                      }
+                      // TODO - an animation of some sort - tr,td cannot set height however.
+                      previousRow = this
+                    }
                   });
                   $('.childOf-' + mlid)
                     .animate({opacity:'1'}, 1500)
-                    // don't use fadeIn because tht acts odd on table elements
-
+                    // don't use fadeIn because tht acts odd on table elements.
                   // Attach any required behaviors to the table
                   // thus, the further child expanders, and the tabledrag again.
-                  // tabledrag doesn't like running twice, so we have to remove some evidence
+                  // tabledrag doesn't like running twice, so we have to remove some evidence.
                   $('#menu-overview').removeClass('tabledrag-processed');
                   $('#menu-overview .tabledrag-handle').remove();
 
@@ -100,7 +99,7 @@ Drupal.behaviors.bigmenu = {
                     return '';
                   };
 
-                  // Indicate we are expanded now
+                  // Indicate we are expanded now.
                   $(parentRow)
                     .removeClass('bigmenu-collapsed')
                     .addClass('bigmenu-expanded');
@@ -109,10 +108,10 @@ Drupal.behaviors.bigmenu = {
                 else {
                   // Failure...
                   alert(Drupal.t('AJAX error fetching submenu.'));
-                  //$('.bigmenu-childindictor', $(parentRow)).remove();
+                  // $('.bigmenu-childindictor', $(parentRow)).remove();
                 }
 
-                // Finished processing, whether success or failure
+                // Finished processing, whether success or failure.
                 $(parentRow)
                   .removeClass('bigmenu-processing')
               },
@@ -124,10 +123,10 @@ Drupal.behaviors.bigmenu = {
           }
           else {
             // This item was already expanded, so a click means it should close.
-            // That means hide the kids
+            // That means hide the kids.
             $('.childOf-' + mlid).css('display', 'none');
 
-            // Indicate we are closed now
+            // Indicate we are closed now.
             $(parentRow)
               .removeClass('bigmenu-processing')
               .removeClass('bigmenu-expanded')
@@ -139,6 +138,6 @@ Drupal.behaviors.bigmenu = {
           return false;
         }
       );
-  }
-};
+    }
+  };
 })(jQuery);
